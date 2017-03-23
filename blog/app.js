af77171c9b8ccb7f58c059ed0bfa765ed261efbf
Alexand Mortensen <alextmortensen@gmail.com>
2017-03-23 17:33:40 -0600

    Added REST routes

--- blog/app.js
+++ blog/app.js
@@ -1,12 +1,14 @@
 var express = require('express'),
     app = express(),
     mongoose = require('mongoose'), 
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost/blog');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 var blogSchema = new mongoose.Schema({
 	title: String,
@@ -16,6 +18,8 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model('Blog', blogSchema);
 
+
+// INDEX
 app.get('/', function(req, res){
 	res.redirect('/blogs');
 });
@@ -30,12 +34,58 @@ app.get('/blogs', function(req, res){
 	});
 });
 
+// NEW
 app.get('/blogs/new', function(req, res){
 	res.render('new');
 });
 
+// CREATE
 app.post('/blogs', function(req, res){
 	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect('/blogs');
+		}
+	});
+});
+
+// SHOW
+app.get('/blogs/:id', function(req, res){
+	Blog.findById(req.params.id, function(err, blog){
+		if(err){
+			console.log(err);
+		} else {
+			res.render('show', {blog: blog});
+		}
+	});
+});
+
+// EDIT
+app.get('/blogs/:id/edit', function(req, res){
+	Blog.findById(req.params.id, function(err, blog){
+		if(err){
+			console.log(err);
+		} else {
+			res.render('edit', {blog: blog});
+		}
+	});
+});
+
+// UPDATE
+app.put('/blogs/:id', function(req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect('/blogs/' + req.params.id);
+		}
+	});
+});
+
+// DESTROY 
+app.delete('/blogs/:id', function(req, res){
+	Blog.findByIdAndRemove(req.params.id, function(err){
 		if(err){
 			console.log(err);
 		} else {
